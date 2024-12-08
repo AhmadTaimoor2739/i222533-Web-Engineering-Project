7db2fd48d59ba1766988178bb899c235e856614d
Ahmed Taimoor <128578237+AhmadTaimoor2739@users.noreply.github.com>
2024-12-09 04:54:20 +0500

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://rift-chief-behavior.glitch.me'
 });
 
 instance.interceptors.request.use(
